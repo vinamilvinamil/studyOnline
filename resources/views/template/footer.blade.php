2f07664f7b5ec3f13c31498ce0377e93fe46662f
vinamilvinamil <huudat.hust@gmail.com>
2015-10-28 12:24:04 +0700

    init

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.2
+    </div>
+    <strong>Demo hệ thống học online.</strong> All rights
+    reserved.
+</footer>
+<!-- jQuery 2.1.3 -->
+<script src="{!! asset ('public/plugins/jQuery/jQuery-2.1.4.min.js') !!}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{!! asset ('public/bootstrap/js/bootstrap.min.js') !!}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{!! asset ('public/dist/js/app.min.js') !!}" type="text/javascript"></script>
---
+++ resources/views/template/head.blade.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{ $page_title or "Online study" }}</title>
+<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+<!-- Bootstrap 3.3.2 -->
+<link href="{!! asset('public/bootstrap/css/bootstrap.min.css') !!}"  rel="stylesheet" type="text/css" />
+<!-- Font Awesome Icons -->
+<link href="{!! asset('public/bootstrap/fontawesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css" />
+<!-- Ionicons -->
+<link href="{!! asset('public/bootstrap/fontionicon/css/ionicons.min.css') !!}" rel="stylesheet" type="text/css" />
+<!-- Theme style -->
+<link href="{!! asset('public/dist/css/AdminLTE.min.css')!!}" rel="stylesheet" type="text/css" />
+<!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+      page. However, you can choose any other skin. Make sure you
+      apply the skin class to the body tag so the changes take effect.
+-->
+<link href="{!! asset('public/dist/css/skins/_all-skins.min.css')!!}" rel="stylesheet" type="text/css" />
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+<![endif]-->
+
